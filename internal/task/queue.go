0a11244dfe54dd41cd47499c5ce07f431bc2c386
团子 <876924196@qq.com>
2026-08-25 13:47:17 +0800

    init: quality baseline

---
+++ internal/task/queue.go
@@ -0,0 +1,45 @@
+package task
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+type Job struct {
+	ID, Kind string
+	Attempts int
+	RunAt    time.Time
+	Done     bool
+}
+type Queue struct {
+	mu   sync.Mutex
+	jobs []Job
+}
+
+func (q *Queue) Enqueue(ctx context.Context, j Job) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	j.RunAt = time.Now()
+	q.jobs = append(q.jobs, j)
+	return nil
+}
+func (q *Queue) Next(ctx context.Context) (Job, bool) {
+	if err := ctx.Err(); err != nil {
+		return Job{}, false
+	}
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	for i, j := range q.jobs {
+		if !j.Done {
+			q.mu.Unlock()
+			q.jobs[i].Done = true
+			q.mu.Lock()
+			return j, true
+		}
+	}
+	return Job{}, false
+}
